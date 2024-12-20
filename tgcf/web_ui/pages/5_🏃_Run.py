22e55553a97140eceb036e9dd18fae877b409d6b
lshcx <luoshenhcx@gmail.com>
2024-12-20 14:33:52 +0800

    Update 5_🏃_Run.py

--- tgcf/web_ui/pages/5_🏃_Run.py
+++ tgcf/web_ui/pages/5_🏃_Run.py
@@ -107,7 +107,7 @@ if check_password(st):
                     CONFIG.agent_fwd_cfg[i].pid = 0
                     write_config(CONFIG)
                     time.sleep(1)
-                    st.experimental_rerun()
+                    st.rerun()
 
                 stop = st.button(f"Stop {agent.alias}", type="primary")
                 if stop:
@@ -134,7 +134,7 @@ if check_password(st):
                 write_config(CONFIG)
                 time.sleep(2)
 
-                st.experimental_rerun()
+                st.rerun()
 
             try:
                 lines = st.slider(
